4f03b47101ff02583887cd2a3a44ccaefb9a0775
llwyd <tom@llwyd.io>
2024-07-29 21:23:53 +0100

    basic version working

--- src/led.h
+++ src/led.h
@@ -6,9 +6,17 @@
 #include <stdio.h>
 #include <stdint.h>
 
+typedef struct
+{ 
+    uint8_t r:8;
+    uint8_t g:8;
+    uint8_t b:8;
+}
+rgb_t;
+
 extern void LED_Init(const char * const device, uint8_t num);
-extern void LED_SetColour(uint8_t led, uint8_t r, uint8_t g, uint8_t b);
-extern void LED_SetAll(uint8_t r, uint8_t g, uint8_t b, uint8_t s);
+extern void LED_SetColour(uint8_t led, const rgb_t * const rgb);
+extern void LED_SetAll(const rgb_t * const rgb, uint8_t s);
 extern void LED_SetBrightness(uint8_t led, uint8_t b);
 extern void LED_Refresh(void);
 extern void LED_Close(void);
--- src/main.c
+++ src/main.c
@@ -2,18 +2,114 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <string.h>
 #include "led.h"
 #include "spi.h"
+#include <unistd.h>
+
+typedef struct
+{
+    rgb_t rgb;
+    uint8_t brightness;
+    uint8_t num_leds;
+    char * device;
+}
+settings_t;
+
+static uint8_t ParseNumberU8(const char * const arg)
+{
+    uint8_t value = 0U;
+    
+    uint32_t raw = (uint32_t)atoi(arg);
+    
+    if(raw > UINT8_MAX)
+    {
+        value = UINT8_MAX;
+    }
+    else
+    {
+        value = (uint8_t)raw;
+    }
+
+    return value;
+}
+
+static bool ParseArgs(int argc, char ** argv, settings_t * settings)
+{
+    int input_flags;
+    bool num_leds_set = false;
+    bool device_set = false;
+    memset(settings, 0x00, sizeof(settings_t));
+    
+    while( ( input_flags = getopt( argc, argv, "r:g:b:s:n:d:" ) ) != -1 )
+    {
+        switch( input_flags )
+        {
+            case 'n':
+            {
+                settings->num_leds = ParseNumberU8(optarg); 
+                num_leds_set = true;
+                break;
+            }
+            case 'r':
+            {
+                settings->rgb.r = ParseNumberU8(optarg); 
+                break;
+            }
+            case 'g':
+            {
+                settings->rgb.g = ParseNumberU8(optarg); 
+                break;
+            }
+            case 'b':
+            {
+                settings->rgb.b = ParseNumberU8(optarg); 
+                break;
+            }
+            case 's':
+            {
+                settings->brightness = ParseNumberU8(optarg); 
+                break;
+            }
+            case 'd':
+            {
+                settings->device = optarg;
+                device_set = true;
+            }
+            default:
+                break;
+        }
+    }
+
+    return (num_leds_set && device_set);
+}
 
 int main(int argc, char **argv)
 {
-    (void)argc;
-    (void)argv;
+    settings_t settings;
+    bool parameters_set = ParseArgs(argc, argv, &settings);
 
-    LED_Init("/dev/spidev0.0", 8U);
-    LED_SetAll(0xFF,0x00,0x00,0xFF);
-    LED_Refresh();
-    LED_Refresh();
-    LED_Close();
+    if(parameters_set)
+    {
+        printf("   device: %s\n", settings.device);
+        printf(" num leds: %u\n", settings.num_leds);
+        printf("intensity: %u\n", settings.brightness);
+        printf("        r: 0x%x\n", settings.rgb.r);
+        printf("        g: 0x%x\n", settings.rgb.g);
+        printf("        b: 0x%x\n", settings.rgb.b);
+    
+#ifdef __arm__
+        LED_Init(settings.device, settings.num_leds);
+        LED_SetAll(&settings.rgb, settings.brightness);
+        LED_Refresh();
+        LED_Refresh();
+        LED_Close();
+#endif
+    }
+    else
+    {
+        printf("Number of LEDs or DEVICE not set\n");
+    }
+    
     return 0U;
 }
